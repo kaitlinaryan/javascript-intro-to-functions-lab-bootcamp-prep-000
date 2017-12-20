6e49345371043683eaf9a746dde92ce24b386cae
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2017-12-20 18:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-function logShout() {
-  console.log('HELLO!')
+function logShout(string) {
+  console.log(string.toUpperCase)
 }
