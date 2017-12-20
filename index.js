7c1fb8c7b08f44fef5806e9d2f3b11a89feed239
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2017-12-20 18:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-if (string.toLowerCase() === string) {
-return "I can\'t hear you!";
+  if (string === string.toLowerCase());
+  var lowerCase = string.toLowerCase();
+  return ('I can\'t hear you!');
+
 }
