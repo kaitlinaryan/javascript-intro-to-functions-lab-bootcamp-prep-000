075189240178e7801259b25e78849052e7846d52
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2017-12-20 18:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function shout(string) {
   return string.toUpperCase ()
 }
-function whisper(string){
+function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
   console.log(string.toUpperCase());
 }
 function logWhisper(string) {
-  console.log(sting.toLowerCase());
+  console.log(string.toLowerCase());
 }
